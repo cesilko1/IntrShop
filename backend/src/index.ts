793b39dfed6614e8720b922b829efef8b27fefcc
Vilém Raška <wilhelm.master@gmail.com>
2022-01-21 10:01:56 +0100

    test DB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,19 +20,21 @@ const MySQL = mysql.createConnection({
 	password: Config.mysql_password
 });
 
+MySQL.connect((error)=>{
+	if(error) {console.error(error); return;}
+	console.log("Connected");
+});
+
 App.use(cors(CorsOptions));
 App.use(express.urlencoded(expressOptions));
 App.use(express.json());
 
-App.get('/', (req: Request, res: Response)=>{
+App.get('/', async (req: Request, res: Response)=>{
+	const response = await MySQL.query('show databases;');
+	console.log(response);
     res.status(200).send("Hello itrshop server");
 });
 
 App.listen(3001, ()=>{
     console.log("Running...");
 });
-
-MySQL.connect((error)=>{
-	if(error) {console.error(error); return;}
-	console.log("Connected");
-})
